e381ee8dc6bd7ad0e338d81fa679207acdaa8d91
essallu <leon.s.liu@ericsson.com>
2013-01-31 17:52:22 +0800

    add a member class.

---
+++ Bekos/src/main/java/com/bekos/form/Member.java
@@ -0,0 +1,76 @@
+package com.bekos.form;
+
+public class Member {
+	
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String password;
+	
+	private String address;
+	
+	private String telephone;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	
+}
